cf9db2fe23884701ff5bee8e2bc40ab0e341bc55
Sandro Park <ticoba@naver.com>
2022-09-23 15:16:44 +0900

    #7 - 스프링부트 프로젝트 시작
    스프링부트 프로젝트 초기화 
    초기 기술 :
    
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/fastcampus/projectboard/FastCampusProjectBoardApplication.java
@@ -0,0 +1,12 @@
+package fastcampus.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCampusProjectBoardApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FastCampusProjectBoardApplication.class, args);
+	}
+
+}
