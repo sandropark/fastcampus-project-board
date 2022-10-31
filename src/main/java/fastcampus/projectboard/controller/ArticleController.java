3110c7fae051cd4c58913d05cad4349b99bddf11
Sandro Park <ticoba@naver.com>
2022-10-31 12:21:39 +0900

    #16 - 게시판 뷰 기본 골격 작성
    게시글 목록 view 기본 골격 작성. 생성한 view의 테스트 통과를 위해서 컨트롤러에 핸들러 메서드 추가.
    테스트에서`MediaType`에 부가적인 옵션이 들어갈 수 있기 때문에 `.contentType`에서 `.contentTypeCompatibleWith`메서드로 변경.

--- src/main/java/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/fastcampus/projectboard/controller/ArticleController.java
@@ -1,10 +1,23 @@
 package fastcampus.projectboard.controller;
 
+import fastcampus.projectboard.domain.Article;
+import fastcampus.projectboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
+    @GetMapping
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 }
--- src/test/java/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -17,13 +17,12 @@ class ArticleControllerTest {
 
     @Autowired MockMvc mvc;
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
