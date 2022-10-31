c96e4d9e97ce22fff016d3cee6ab2cfd2da17e6c
Sandro Park <ticoba@naver.com>
2022-10-31 13:52:44 +0900

    #16 - 게시글 뷰 기본 골격 작성
    뷰, 컨트롤러, 테스트까지 한 번에 작성

--- src/main/java/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/fastcampus/projectboard/controller/ArticleController.java
@@ -1,12 +1,9 @@
 package fastcampus.projectboard.controller;
 
-import fastcampus.projectboard.domain.Article;
-import fastcampus.projectboard.repository.ArticleRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,4 +17,12 @@ public class ArticleController {
         map.addAttribute("articles", List.of());
         return "articles/index";
     }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("article", "1");  // TODO : 구현할 때 실제 데이터를 바인딩하기.
+        map.addAttribute("articleComments", List.of());
+        return "articles/detail";
+    }
+
 }
