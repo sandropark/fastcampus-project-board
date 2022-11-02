8ef940a089e53c1fedb28ab1354221a8d7ef338a
Sandro Park <ticoba@naver.com>
2022-11-02 14:20:46 +0900

    #34 - 게시판 페이지 검색 타입을 전달

--- src/main/java/fastcampus/projectboard/domain/type/SearchType.java
+++ src/main/java/fastcampus/projectboard/domain/type/SearchType.java
@@ -1,5 +1,17 @@
 package fastcampus.projectboard.domain.type;
 
+import lombok.Getter;
+
 public enum SearchType {
-    TITLE, CONTENT, HASHTAG, ID, NICKNAME
+    TITLE("제목"),
+    CONTENT("본문"),
+    HASHTAG("해시태그"),
+    ID("유저 id"),
+    NICKNAME("닉네임");
+
+    @Getter private final String description;
+
+    SearchType(String description) {
+        this.description = description;
+    }
 }
